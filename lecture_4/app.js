934d95162b177e7d7c0c6aaa3f6379932208bce6
prince-gh <pwskills@outlook.com>
2024-10-13 18:26:32 +0530

    Learn .Controller, use-strict

---
+++ lecture_4/app.js
@@ -0,0 +1,11 @@
+( function () {
+    'use strict'
+    
+
+    let userName = "Prince";
+    
+    angular.module('myFirstApp', [])
+    .controller('myFirstController', function (){
+
+    })
+} )();

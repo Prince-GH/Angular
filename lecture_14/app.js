0bebfeb53a452dcc42a3bc670896c9f143ced75c
prince-gh <pwskills@outlook.com>
2024-10-22 13:56:21 +0530

    Add digest '$'watcher

--- lecture_14/app.js
+++ lecture_14/app.js
@@ -18,22 +18,28 @@
         $scope.OnceCount = function () {
             $scope.Count = 1;
         }
-
+        
         $scope.OnceCountIncrement = function () {
             $scope.CountInc++;
         }
 
-
-        //Watcher 1 to focus on Count
-        $scope.$watch('Count',function (newVal, oldVal) {
-            console.log("Old Value: ",oldVal);
-            console.log("New Value: ",newVal);
-        })
+        $scope.MyName = "Ravi";
         
-        //Watcher 2 to focus on CountInc
-        $scope.$watch('CountInc', (newVal, oldVal)=>{
-            console.log("Inc newVal: ",newVal);
-            console.log("Inc oldVal: ",oldVal);
+        $scope.$watch(()=>{
+            console.log("Watcher Blast!! 🔥");
         })
+
+
+        // //Watcher 1 to focus on Count
+        // $scope.$watch('Count',function (newVal, oldVal) {
+        //     console.log("Old Value: ",oldVal);
+        //     console.log("New Value: ",newVal);
+        // })
+        
+        // //Watcher 2 to focus on CountInc
+        // $scope.$watch('CountInc', (newVal, oldVal)=>{
+        //     console.log("Inc newVal: ",newVal);
+        //     console.log("Inc oldVal: ",oldVal);
+        // })
     }
 })();
